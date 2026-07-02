0fa9260a748c48aec57b9dbe4db8c4e71cdd1199
TrueKing <your@email.com>
2026-07-01 20:43:43 -0400

    bookmarks: add video speed and profiles, fix design mode syntax

--- bookmarks.nix
+++ bookmarks.nix
@@ -58,11 +58,21 @@
         url = "about:config";
         keyword = "c";
       }
-
       {
-        name = "Design Mode"
+        name = "Design Mode";
         url = "javascript:(function(){if(document.designMode==='on'){document.designMode='off';document.body.contentEditable='false';}else{document.designMode='on';document.body.contentEditable='true';}})();";
-        keyword = "dm"
+        keyword = "dm";
+      }
+      {
+        name = "Video";
+        url = "javascript:(function(){var v=document.querySelector('video');if(v){var s=prompt('Enter playback speed:',v.playbackRate);if(s){v.playbackRate=parseFloat(s);}}else{alert('No video found on this page.');}})();";
+        keyword = "vp";
+
+      }
+      {
+        name = "Profile";
+        url = "about:profiles";
+        keyword = "p";
       }
     
     ];
