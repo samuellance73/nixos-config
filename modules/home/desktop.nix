f54482b19bd4c698ade2dc52994115a9b0e550a1
TrueKing <your@email.com>
2026-07-08 11:50:46 -0400

    fix: restore Bibata-Modern-Ice cursor theme
    - Add home.pointerCursor to install cursor into user profile
    - Add HYPRCURSOR_THEME/SIZE env vars for Hyprland 0.55 compatibility
    - Hyprland 0.55 now prioritises hyprcursor over xcursor

--- modules/home/desktop.nix
+++ modules/home/desktop.nix
@@ -33,6 +33,18 @@ in
 {
   fonts.fontconfig.enable = true;
 
+  home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+    hyprcursor = {
+      enable = true;
+      size = 24;
+    };
+  };
+
   home.packages = with pkgs; [
     htop grim slurp libnotify hyprpolkitagent hyprlock
     waybar bluez brightnessctl networkmanagerapplet micro
